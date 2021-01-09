26a0d0376c1b6d7ae15f652ece63cdbae8f8200c
Alec Wolyniec <alecwolyniec@gmail.com>
2021-01-08 19:04:01 -0500

    feat: Initialize server; receive connections, sign-in messages, and broadcast sign-in messages

---
+++ server.js
@@ -0,0 +1,47 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+// TODO: after server start, make sure all clients have to sign in right after connecting
+// TODO: if a client drops, log them out
+// TODO: implement origin-checking
+
+const USERS = []; // in-memory data store
+
+const broadcastMessageToAllUsers = (message) => {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+};
+
+const handleUserEnterChat = (ws, message) => {
+  const { data: { userName, color } } = message;
+  const isUserUnique = !USERS.find(user => user.name === userName);
+  if (isUserUnique) {
+    console.log(`User entering chat: ${JSON.stringify(message, null, 2)}`);
+    USERS.push({
+      name: userName,
+      color
+    });
+    // inform all users that a user has entered (and confirm to the logged-in user that they have signed in)
+    broadcastMessageToAllUsers(JSON.stringify({
+      type: 'userEnterChat',
+      userName,
+      color
+    }));
+  } else {
+    console.error(`Non-unique user trying to enter: ${JSON.stringify(message, null, 2)}`);
+  }
+};
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    const messageJSON = JSON.parse(message);
+    const { type } = messageJSON;
+    if (type === 'userEnterChat') {
+      handleUserEnterChat(ws, messageJSON);
+    }
+  });
+});
